2d8ff8f4d8fc1db9dfb8dfd204843961817f8136
Lukasz Parulski <lukaszparulski@wp.pl>
2021-11-03 00:01:51 +0100

    Mock generator

---
+++ generator/generator.py
@@ -0,0 +1,12 @@
+﻿import pika
+
+print("TEST")
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.local', port=5672))
+channel = connection.channel()
+
+channel.queue_declare(queue='test_queue')
+
+for i in range(10):
+    channel.basic_publish(exchange='', routing_key='test_queue', body='Message no ' + str(i+1))
+    print(" [x] Sent message no " + str(i+1))
+connection.close()
